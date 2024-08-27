9c2a5d5f9e20fd9cfb94dc9ef06d35024af3eac0
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-08-26 21:01:47 -0300

    chore: export types

--- src/index.ts
+++ src/index.ts
@@ -3,36 +3,5 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { exec } from "child_process";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-import { DecompilerArgs, DecompilerArgsKey } from "./interfaces/decompiler.js";
-import { toOriginalCase } from "./utils/to-original-case.js";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const rootPath = join(__dirname, "..");
-const extractPath = join(rootPath, "Decompiler");
-const executablePath = join(extractPath, "Decompiler");
-
-export function vrfDecompiler({ debug, ...args }: DecompilerArgs): Promise<string> {
-    return new Promise((resolve, reject) => {
-        const formattedArgs = Object.entries(args)
-            .map(([key, value]) => {
-                key = toOriginalCase(key as DecompilerArgsKey);
-                return typeof value === "boolean" ? key : `${key} ${value}`;
-            })
-            .join(" ");
-        const command = `${executablePath} ${formattedArgs}`;
-        if (debug) {
-            console.log(command);
-        }
-        exec(command, { maxBuffer: 1024 * 1024 * 50 }, (error, stdout) => {
-            if (error) return reject(error);
-            resolve(stdout);
-        });
-    });
-}
-
-export function resolvePath(...paths: string[]): string {
-    return join(rootPath, ...paths);
-}
+export * from "./interfaces/decompiler.js";
+export * from "./vrf-decompiler.js";
