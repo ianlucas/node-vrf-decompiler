0d8579f17cfc83e859b5ffa8de60c39ea7eb237b
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-08-28 23:42:49 -0300

    feat: use `spawn` instead of `exec`

--- src/vrf-decompiler.ts
+++ src/vrf-decompiler.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { exec } from "child_process";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import { DecompilerArgs, DecompilerArgsKey } from "./interfaces/decompiler.js";
@@ -14,25 +14,13 @@ const rootPath = join(__dirname, "..");
 const extractPath = join(rootPath, "Decompiler");
 const executablePath = join(extractPath, "Decompiler");
 
-export function vrfDecompiler({ debug, ...args }: DecompilerArgs): Promise<string> {
-    return new Promise((resolve, reject) => {
-        const formattedArgs = Object.entries(args)
-            .map(([key, value]) => {
-                key = toOriginalCase(key as DecompilerArgsKey);
-                return typeof value === "boolean" ? key : `${key} ${value}`;
-            })
-            .join(" ");
-        const command = `${executablePath} ${formattedArgs}`;
-        if (debug) {
-            console.log(command);
-        }
-        exec(command, { maxBuffer: 1024 * 1024 * 50 }, (error, stdout) => {
-            if (error) return reject(error);
-            resolve(stdout);
-        });
+export function vrfDecompiler({ debug, ...args }: DecompilerArgs): ChildProcessWithoutNullStreams {
+    const formattedArgs = Object.entries(args).flatMap(([key, value]) => {
+        key = toOriginalCase(key as DecompilerArgsKey);
+        return typeof value === "boolean" ? [key] : [key, value];
     });
-}
-
-export function resolvePath(...paths: string[]): string {
-    return join(rootPath, ...paths);
+    if (debug) {
+        console.log(`${executablePath} ${formattedArgs.join(" ")}`);
+    }
+    return spawn(executablePath, formattedArgs);
 }
